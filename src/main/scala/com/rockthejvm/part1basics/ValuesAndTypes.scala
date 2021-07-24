c89ddc76c43e6de4017fc35225c82c652adebb3c
ndesai <neelharshadkumar.desai@here.com>
2021-07-24 16:13:49 -0500

    Jay Bahuchar:First Commit

---
+++ src/main/scala/com/rockthejvm/part1basics/ValuesAndTypes.scala
@@ -0,0 +1,25 @@
+package com.rockthejvm.part1basics
+
+object ValuesAndTypes {
+
+  // values
+  val jaybahucharmataji: String = "Jay Bahuchar Mataji"
+
+  // Type inferrence
+  val anInteger = 67 // : Int is optional
+
+  // common types
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = 78 // 4 bytes
+  val aShort: Short = 5263 // 2 bytes
+  val aLong: Long = 583583532225L // 8 bytes
+  val aFloat: Float = 2.4f // 4 bytes
+  val aDouble: Double = 3.14 // 8 bytes
+
+  // string
+  val aString: String = "Scala"
+  def main(args: Array[String]): Unit = {
+
+  }
+}
---
+++ src/main/scala/com/rockthejvm/playground/Playground.scala
@@ -0,0 +1,7 @@
+package com.rockthejvm.playground
+
+object Playground {
+  def main(args: Array[String]): Unit = {
+    println("Jay Bahuchar Mataji. Learning Scala 3!!!!!!!!!!!")
+  }
+}
